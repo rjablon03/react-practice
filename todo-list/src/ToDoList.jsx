5661a67b3cac382be0bfedc2fa9e285e16e5c9a6
rjablon03 <secrets_crasser.0l@icloud.com>
2025-01-02 19:13:38 -0500

    Started working on the list logic

--- todo-list/src/ToDoList.jsx
+++ todo-list/src/ToDoList.jsx
@@ -4,17 +4,39 @@ function ToDoList() {
     const [taskList, setTaskList] = useState([]);
     const [newTask, setNewTask] = useState("");
 
+    const handleNewTask = (e) => {
+        setNewTask(e.target.value);
+    }
+
+    const addNewTask = () => {
+        if (taskList.length === 0) {
+            document.querySelector(".default-message").style.display = "none";
+        }
+
+        setTaskList([...taskList, newTask]);
+        setNewTask("");
+        document.querySelector(".create-task input").value = "";
+    }
+
     return (
         <div className="container">
             <h1>To-Do List</h1>
 
             <div className="create-task">
-                <input type="text" placeholder="Enter a new task" />
-                <button>Add</button>
+                <input type="text" placeholder="Enter a new task" onChange={handleNewTask}/>
+                <button onClick={addNewTask}>Add</button>
             </div>
 
             <div className="task-list">
-                <p>You have no tasks</p>
+                <p className="default-message">You have no tasks</p>
+                {taskList.map((task, index) => {
+                    return (
+                        <div key={index} className="task">
+                            <input type="checkbox"/>
+                            <span>{task}</span>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
